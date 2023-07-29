dbd84ae34127c070c5cb6a7fd8b23a14e7457daf
rakshitha007 <79582783+rakshitha007@users.noreply.github.com>
2023-07-29 23:37:12 +0530

    digiclock.c

--- digiclock.c
+++ digiclock.c
@@ -8,22 +8,28 @@ int main()
 	system("cls"); 
 	printf("Set time (HR MIN SEC):\n");
 	scanf("%d%d%d",&hr,&min,&sec);
-	while(1) {
-		system("cls");
-		printf("%02d:%02d:%02d",hr,min,sec);
-		sec++;
-		if(sec==60) {	
-			min++;
-			sec=0;
+
+	if(hr < 24 && min < 60 && sec < 60) {
+		while(1) {
+			system("cls");
+			printf("%02d:%02d:%02d",hr,min,sec);
+			sec++;
+			if(sec==60) {	
+				min++;
+				sec=0;
+			}
+			if(min==60) {
+				hr++;
+				min=0;
+			}
+			if(hr==24) {
+				hr=min=sec=0;
+			}
+			sleep(1);
 		}
-		if(min==60) {
-			hr++;
-			min=0;
-		}
-		if(hr==24) {
-			hr=min=sec=0;
-		}
-		sleep(1);
+	} else {
+		printf("Invalid input!!!");
 	}
+	
 	return 0;
 }
