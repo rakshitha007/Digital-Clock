1aa7f0946c88354dc526b4d83a0bb180e484cbf7
rakshitha007 <79582783+rakshitha007@users.noreply.github.com>
2023-07-29 19:21:34 +0530

    digiclock.c

--- digiclock.c
+++ digiclock.c
@@ -1,34 +1,27 @@
 #include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
 #include<unistd.h>
+#include<conio.h>
+
 int main()
 {
 	int hr,min,sec;
-	system("clear"); //clears output screen
-	printf("Set time :\n");
+	system("cls"); 
+	printf("Set time (HR MIN SEC):\n");
 	scanf("%d%d%d",&hr,&min,&sec);
-	while(1)
-	{
-		system("clear");
+	while(1) {
+		system("cls");
 		printf("%02d:%02d:%02d",hr,min,sec);
-		fflush(stdout);
 		sec++;
-		if(sec==60)
-		{	
+		if(sec==60) {	
 			min++;
 			sec=0;
 		}
-		if(min==60)
-		{
+		if(min==60) {
 			hr++;
-			min++;
-		}
-		if(hr==24)
-		{
-			hr=0;
 			min=0;
-			sec=0;
+		}
+		if(hr==24) {
+			hr=min=sec=0;
 		}
 		sleep(1);
 	}
